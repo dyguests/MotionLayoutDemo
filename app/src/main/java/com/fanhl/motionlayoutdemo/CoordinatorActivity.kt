756b8ee3b9bafc2007a66319fbf708ca1403fdc0
fanhl <dyguests@qq.com>
2018-10-27 22:14:22 +0800

    header ok

--- app/src/main/java/com/fanhl/motionlayoutdemo/CoordinatorActivity.kt
+++ app/src/main/java/com/fanhl/motionlayoutdemo/CoordinatorActivity.kt
@@ -1,12 +1,16 @@
 package com.fanhl.motionlayoutdemo
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import kotlinx.android.synthetic.main.partial_coordinator_header.*
 
 class CoordinatorActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_coordinator)
+
+        //是否显示路径
+        constraintToolbar.setShowPaths(true)
     }
 }
