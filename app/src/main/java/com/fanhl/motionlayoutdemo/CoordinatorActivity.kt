ffac6c650041e0ac25f476d19002c359cb790e9a
fanhl <dyguests@qq.com>
2018-10-27 21:30:19 +0800

    CollapsibleToolbar

---
+++ app/src/main/java/com/fanhl/motionlayoutdemo/CoordinatorActivity.kt
@@ -0,0 +1,12 @@
+package com.fanhl.motionlayoutdemo
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class CoordinatorActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_coordinator)
+    }
+}
