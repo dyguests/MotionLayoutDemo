b2c9d2b4fe3bdf5aef97adfbae5f7aa5e645e940
fanhl <dyguests@qq.com>
2018-10-27 18:11:00 +0800

    fab

--- app/src/main/java/com/fanhl/motionlayoutdemo/MainActivity.kt
+++ app/src/main/java/com/fanhl/motionlayoutdemo/MainActivity.kt
@@ -2,11 +2,18 @@ package com.fanhl.motionlayoutdemo
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.transition.TransitionManager
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        fab_1.setOnClickListener {
+            TransitionManager.beginDelayedTransition(root)
+            constraints_1.constraintSet.applyTo(root)
+        }
     }
 }
