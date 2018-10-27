28c8ef2e3f842d65d868551f08182ccf63cd4e09
fanhl <dyguests@qq.com>
2018-10-27 20:26:39 +0800

    mine

---
+++ app/src/main/java/com/fanhl/motionlayoutdemo/MineActivity.kt
@@ -0,0 +1,22 @@
+package com.fanhl.motionlayoutdemo
+
+import android.os.Bundle
+import android.support.design.widget.Snackbar
+import android.support.v7.app.AppCompatActivity
+import android.view.WindowManager
+import kotlinx.android.synthetic.main.activity_mine.*
+
+class MineActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        window.setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS)
+        setContentView(R.layout.activity_mine)
+        setSupportActionBar(toolbar)
+
+        fab.setOnClickListener { view ->
+            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                    .setAction("Action", null).show()
+        }
+    }
+}
